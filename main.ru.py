e9f2f9f99f88c33b1b9896695d7a07b6f1f7a055
As10W <alexander4198@mail.ru>
2025-01-15 13:29:58 +0300

    25.12.2024

--- main.ru.py
+++ main.ru.py
@@ -1,1 +1,12 @@
-print('Hello')
+username = 'Дмитрий'
+print(username)
+title = 'Покупка продуктов'
+print(title)
+content = 'Купить шоколад,жвачка и вода.'
+print(content)
+status = 'active'
+print(status)
+created_date =  "10-01-2025"
+print(created_date)
+issue_date = "10-02-2025"
+print(issue_date)
